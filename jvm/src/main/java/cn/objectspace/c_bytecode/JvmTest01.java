0154913a70c93c3ce1ecfd5bb864adabdab42ccd
NoCortY <nocorty@163.com>
2020-10-20 23:18:47 +0800

    解析class文件

--- jvm/src/main/java/cn/objectspace/c_bytecode/JvmTest01.java
+++ jvm/src/main/java/cn/objectspace/c_bytecode/JvmTest01.java
@@ -24,21 +24,22 @@ public class JvmTest01 {
          * 网络传输是大端模式
          *
          * class文件的组成
-         * 1.魔数：cafe babe
-         * 2.主版本号：major  JDK1.8的主版本号是52
-         * 3.次版本号：minor  JDK1.8的次版本号是0  用于控制不同JDK版本编译的程序是否能在不同的JDK跑起来
-         * 4.常量池：三种  class中的常量池（静态的）   运行时常量池（HSDB 动态的） 字符串常量池（StringTable）
-         * 5.类的访问控制权限
-         * 6.类名：this_class
-         * 7.父类名：super_class
-         * 8.实现的接口个数:interface_count  一个类最多能实现65535个接口
-         * 9.实现的接口 interface[]
-         * 10.属性数量：fields_count
-         * 11.属性值：fields_info[]
-         * 12.methods_count：方法数量
-         * 13.method_info[]:方法值
-         * 14.attributes_count:属性数量
-         * 15.attributes[]:值
+         * 1.u4魔数：cafe babe  加载class时校验是否存在魔数，判断是否是正常class文件
+         * 2.u2主版本号：major  JDK1.8的主版本号是52
+         * 3.u2次版本号：minor  JDK1.8的次版本号是0  用于控制不同JDK版本编译的程序是否能在不同的JDK跑起来
+         * 4.u2常量池(最小index是1所以最终数量需要减一)：三种  class中的常量池（静态的）   运行时常量池（HSDB 动态的） 字符串常量池（StringTable）
+         * 5.!常量池
+         * 6.u2类的访问控制权限
+         * 7.u2类名：this_class
+         * 8.u2父类名：super_class
+         * 9.u2实现的接口个数:interface_count  一个类最多能实现65535个接口
+         * 10.!实现的接口 interface[]
+         * 11.u2属性数量：fields_count
+         * 12.!属性值：fields_info[]
+         * 13.u2方法数量:umethods_count
+         * 14.！方法值:method_info[]
+         * 15.u2属性的属性数量：attributes_count
+         * 16.属性的属性值：attributes[]
          */
     }
 }
